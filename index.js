9e2e36a6f64cd6e012ea714bb25299729f174917
さかもと <124328826+skmtoo@users.noreply.github.com>
2025-02-04 16:13:31 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// 'root' 要素を取得
+const rootElement = document.getElementById("root");
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// 'root' 要素が見つからない場合のエラーハンドリングを強化
+if (!rootElement) {
+  console.error("エラー: 'root' の要素が見つかりません。index.html を確認してください。");
+} else {
+  const root = ReactDOM.createRoot(rootElement);
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+
+  // パフォーマンス計測（必要であれば有効化）
+  reportWebVitals(console.log);
+}
